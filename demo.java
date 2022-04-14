aad4a28a224778689fcf7871d3ea0365cfb5df49
bhupendrargoud <90169332+bhupendrargoud@users.noreply.github.com>
2022-04-14 16:40:10 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,35 @@
+mafcat          bhupendra_rakesh
+		Br805011
+
+windows		bhupendra_rakesh
+		Mar032022
+
+fotieclient	bhupendra_rakesh
+		Mar032022
+		token
+new pass	bhup123$
+
+iportal 	6403
+		8050@Br11
+
+
+		https://labs.alchemycloud.co.in
+alcemy		user450
+		Cloud@123
+		welcome
+
+mysql		Br8050
+
+mysql server	Br8050888
+
+UAN		101737954155
+		Ravi@8050
+
+learnlytica 	bhupendra_rakesh@gmail.com
+		password
+
+docker hub	bhupendrargoud
+		Br@8050885534
+
+
+aws		bhupendrargoud@gmail.com
